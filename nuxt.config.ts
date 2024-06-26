b8761e2093938baad1f3fa2919d4313015c6e92d
Jordan Kniest <j.kniest@basecom.de>
2024-06-26 15:58:43 +0200

    Install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt'
+  ],
   experimental: {
     asyncContext: true,
     sharedPrerenderData: true,
