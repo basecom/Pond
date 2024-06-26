e7c9db60797557de3ce56e2c5637d48954bebbfa
Jordan Kniest <j.kniest@basecom.de>
2024-06-26 16:07:40 +0200

    Add radix vue

--- app.vue
+++ app.vue
@@ -1,7 +1,27 @@
+<script setup lang="ts">
+import { ProgressIndicator, ProgressRoot } from 'radix-vue'
+
+const progressValue = ref(20)
+setTimeout(() => {
+  progressValue.value = 100
+}, 2000)
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     
     <h1 class="text-3xl font-semibold text-blue-600">Pond.</h1>
+
+    <ProgressRoot
+    v-model="progressValue"
+    class="relative overflow-hidden bg-blue-600 rounded-full w-full sm:w-[300px] h-4 sm:h-5"
+    style="transform: translateZ(0)"
+  >
+    <ProgressIndicator
+      class="bg-blue-200 rounded-full w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+      :style="`transform: translateX(-${100 - progressValue}%)`"
+    />
+  </ProgressRoot>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@formkit/auto-animate/nuxt',
     '@vueuse/nuxt',
+    'radix-vue/nuxt',
   ],
   experimental: {
     asyncContext: true,
