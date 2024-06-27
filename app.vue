522baf089fa771d1bca942499e4b8ba05408d8ea
nele sophie <53444487+aheartforspinach@users.noreply.github.com>
2024-06-27 14:24:39 +0200

    Feature/prettier tailwind (#2)
    * add prettier, tailwind prettier, adjust eslint
    
    * add tailwind plugin
    
    * use single quotes
    
    ---------
    
    Co-authored-by: neleruess <n.ruess@basecom.de>

--- app.vue
+++ app.vue
@@ -1,27 +1,27 @@
 <script setup lang="ts">
-import { ProgressIndicator, ProgressRoot } from 'radix-vue'
+import { ProgressIndicator, ProgressRoot } from 'radix-vue';
 
-const progressValue = ref(20)
+const progressValue = ref(20);
 setTimeout(() => {
-  progressValue.value = 100
-}, 2000)
+    progressValue.value = 100;
+}, 2000);
 </script>
 
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    
-    <h1 class="text-3xl font-semibold text-blue-600">Pond.</h1>
+    <div>
+        <NuxtRouteAnnouncer />
 
-    <ProgressRoot
-    v-model="progressValue"
-    class="relative overflow-hidden bg-blue-600 rounded-full w-full sm:w-[300px] h-4 sm:h-5"
-    style="transform: translateZ(0)"
-  >
-    <ProgressIndicator
-      class="bg-blue-200 rounded-full w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
-      :style="`transform: translateX(-${100 - progressValue}%)`"
-    />
-  </ProgressRoot>
-  </div>
+        <h1 class="text-3xl font-semibold text-blue-600">Pond.</h1>
+
+        <ProgressRoot
+            v-model="progressValue"
+            class="relative h-4 w-full overflow-hidden rounded-full bg-blue-600 sm:h-5 sm:w-[300px]"
+            style="transform: translateZ(0)"
+        >
+            <ProgressIndicator
+                class="ease-[cubic-bezier(0.65, 0, 0.35, 1)] h-full w-full rounded-full bg-blue-200 transition-transform duration-[660ms]"
+                :style="`transform: translateX(-${100 - progressValue}%)`"
+            />
+        </ProgressRoot>
+    </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxtjs/tailwindcss',
-    '@formkit/auto-animate/nuxt',
-    '@vueuse/nuxt',
-    'radix-vue/nuxt',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-  ],
-  experimental: {
-    asyncContext: true,
-    sharedPrerenderData: true,
-    typedPages: true,
-    viewTransition: true,
-  },
-  features: {
-    devLogs: true
-  },
-  future: {
-    compatibilityVersion: 4
-  }
-})
+    devtools: { enabled: true },
+    modules: [
+        '@pinia/nuxt',
+        '@nuxtjs/tailwindcss',
+        '@formkit/auto-animate/nuxt',
+        '@vueuse/nuxt',
+        'radix-vue/nuxt',
+        '@nuxt/eslint',
+        '@nuxt/fonts',
+    ],
+    experimental: {
+        asyncContext: true,
+        sharedPrerenderData: true,
+        typedPages: true,
+        viewTransition: true,
+    },
+    features: {
+        devLogs: true,
+    },
+    future: {
+        compatibilityVersion: 4,
+    },
+});
