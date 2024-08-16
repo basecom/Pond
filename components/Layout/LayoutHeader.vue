9fcfb4417b2dacdf10dd5392da9dbd4ffc149b7a
Marie Löffler <m.loeffler@basecom.de>
2024-08-16 07:59:27 +0200

    refactor popover to own component

--- components/Layout/LayoutHeader.vue
+++ components/Layout/LayoutHeader.vue
@@ -94,35 +94,30 @@ const isActive = (path: Schemas['SeoUrl'][] | null) => {
                     <!-- account -->
                     <LazySharedModal
                         v-if="!signedIn"
-                        :controller="modalController"
                         :with-close-button="true"
-                        class="w-1/3"
                     >
+                        <template #trigger>
+                            <FormKitIcon
+                                class="block h-6 w-6"
+                                icon="user"
+                            />
+                        </template>
                         <template #title>Login</template>
                         <template #content>
                             <AccountLoginRegisterTabs />
                         </template>
                     </LazySharedModal>
-                    <PopoverRoot>
-                        <PopoverTrigger
-                            class="none inline-flex h-8 w-8 items-center justify-center"
-                            aria-label="account dropdown"
-                        >
-                            <!-- icon in header -->
+                    <LazySharedPopover v-else>
+                        <template #trigger>
                             <FormKitIcon
                                 class="block h-6 w-6"
                                 icon="user"
                                 @click="!signedIn ? modalController.open() : null"
                             />
-                        </PopoverTrigger>
-                        <PopoverContent
-                            v-if="signedIn"
-                            side="bottom"
-                            :side-offset="5"
-                            class="w-64 rounded border border-gray-light bg-white p-4 shadow-md"
-                        >
+                        </template>
+                        <template #content>
                             <div class="py-2 first:pt-0">
-                                <NuxtLink to="/account"> account </NuxtLink>
+                                <NuxtLink to="/account">account</NuxtLink>
                             </div>
                             <FormKit
                                 type="submit"
@@ -131,18 +126,8 @@ const isActive = (path: Schemas['SeoUrl'][] | null) => {
                             >
                                 logout
                             </FormKit>
-                            <PopoverClose
-                                class="absolute right-1 top-1 inline-flex h-6 w-6 items-center justify-center rounded-full"
-                                aria-label="Close"
-                            >
-                                <FormKitIcon
-                                    class="block h-5 w-5 text-gray"
-                                    icon="x"
-                                />
-                            </PopoverClose>
-                            <PopoverArrow class="fill-white" />
-                        </PopoverContent>
-                    </PopoverRoot>
+                        </template>
+                    </LazySharedPopover>
 
                     <!-- cart -->
                     <button
