26ab9d5a56f88826535e80b1a2faeb0fbc0ed47c
Marie Löffler <m.loeffler@basecom.de>
2024-08-23 14:44:33 +0200

    add product name, manufacturer placeholder

---
+++ components/cms/element/CmsElementManufacturerLogo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import type { CmsElementManufacturerLogo } from '@shopware-pwa/composables-next';
+
+defineProps<{
+    element: CmsElementManufacturerLogo;
+}>();
+</script>
+
+<template>
+    manufacturer (currently data not provided properly)
+</template>
---
+++ components/cms/element/CmsElementProductName.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import type { CmsElementProductName } from '@shopware-pwa/composables-next';
+
+defineProps<{
+    element: CmsElementProductName;
+}>();
+</script>
+
+<template>
+    <h1>
+        {{ element.data.content }}
+    </h1>
+</template>
