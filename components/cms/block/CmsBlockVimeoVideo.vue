2023c6eebfce2b37af90baa8a620be2a103c28a9
carojansing <c.jansing@basecom.de>
2024-09-11 15:25:43 +0200

    BUS-825-CMS-Vimeo-Element

---
+++ components/cms/block/CmsBlockVimeoVideo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Schemas } from '@shopware/api-client/api-types';
+import CmsLoader from '~/components/cms/CmsLoader.vue';
+
+const props = defineProps<{
+    block: Schemas['CmsBlock'];
+}>();
+
+const { getSlotContent } = useCmsBlock(props.block);
+
+const video: Schemas['CmsSlot'] = getSlotContent('video');
+</script>
+
+<template>
+    <CmsLoader :content="video" />
+</template>
---
+++ components/cms/element/CmsElementVimeoVideo.vue
@@ -0,0 +1,30 @@
+<style>
+#vimeo-video {
+}
+</style>
+<script setup lang="ts">
+import type { CmsElementVimeoVideo } from '@shopware-pwa/composables-next';
+import Player from '@vimeo/player';
+const props = defineProps<{
+    element: CmsElementVimeoVideo;
+}>();
+console.log(props.element);
+const vimeoVideo = ref(null)
+onMounted(()=> {
+    if (vimeoVideo.value){
+        const player = new Player('vimeo-video', {
+            id: parseInt(props.element.config.videoID.value),
+            width: vimeoVideo.value?.offsetWidth ?? null
+        });
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+}
+})
+</script>
+<template>
+<div ref="vimeoVideo">
+    <div id="vimeo-video">
+    </div>
+</div>
+</template>
