65caba1b622b75322ed4aaa5340e970ad8873c94
Marius Kruggel <m.kruggel@basecom.de>
2024-09-16 10:15:41 +0200

    [BUS-806] add newsletter un/subscribe functionality

--- components/Layout/Account/LayoutAccountOverviewContent.vue
+++ components/Layout/Account/LayoutAccountOverviewContent.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
 const customerStore = useCustomerStore();
+const {
+    isNewsletterSubscriber,
+    newsletterSubscribe,
+    newsletterUnsubscribe,
+    getNewsletterStatus,
+} = useNewsletter();
+
+onMounted(() => {
+    getNewsletterStatus();
+});
+
+const handleNewsletterChange = async (event: Event) => {
+    const checked = (event.target as HTMLInputElement).checked;
+        if (checked) {
+            await newsletterSubscribe({
+                email: customerStore.customer.email,
+                option: 'subscribe',
+            });
+        } else {
+            await newsletterUnsubscribe(customerStore.customer.email);
+        }
+};
+
 </script>
 
 <template>
     <h1 class="font-bold mb-2">Overview</h1>
-    <div class="mb-4 text-base">Directly access your profile information, the default payment method and given addresses.</div>
-    <div v-if="customerStore.customer"
-        class="grid grid-cols-1 gap-6 lg:grid-cols-2 col-span-2">
+    <div class="mb-4 text-base">
+        Directly access your profile information, the default payment method and given addresses.
+    </div>
+
+    <div v-if="customerStore.customer" class="grid grid-cols-1 gap-6 lg:grid-cols-2 col-span-2">
         <div class="bg-white p-4 rounded-lg shadow-md">
             <h3 class="text-lg font-semibold mb-2">Personal Information</h3>
             <p><strong>Name:</strong> {{ customerStore.customer.firstName }} {{ customerStore.customer.lastName }}</p>
@@ -19,8 +44,8 @@ const customerStore = useCustomerStore();
             <p><strong>Description:</strong> {{ customerStore.customer.defaultPaymentMethod.description }}</p>
         </div>
     </div>
-    <div v-if="customerStore.customer"
-         class="grid grid-cols-1 gap-6 mt-4 lg:grid-cols-2 col-span-2">
+
+    <div v-if="customerStore.customer" class="grid grid-cols-1 gap-6 mt-4 lg:grid-cols-2 col-span-2">
         <div class="bg-white p-4 rounded-lg shadow-md">
             <h3 class="text-lg font-semibold mb-2">Billing Address</h3>
             <p>{{ customerStore.customer.defaultBillingAddress.street }}</p>
@@ -35,8 +60,12 @@ const customerStore = useCustomerStore();
             <p>{{ customerStore.customer.defaultShippingAddress.country.name }}</p>
         </div>
     </div>
+
+    <div v-if="customerStore.customer" class="bg-white p-4 mt-4 rounded-lg shadow-md">
+        <h3 class="text-lg font-semibold mb-2">Newsletter Subscription</h3>
+        <label>
+            <input type="checkbox" :checked="isNewsletterSubscriber" @change="handleNewsletterChange" />
+            Yes, I would like to subscribe to the free Demostore newsletter. (I may unsubscribe at any time.)
+        </label>
+    </div>
 </template>
-
-<style scoped>
-
-</style>
