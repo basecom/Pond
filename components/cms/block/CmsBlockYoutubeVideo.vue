9a5abd0f23808a390c9c313747d9318ee5392a65
carojansing <c.jansing@basecom.de>
2024-09-17 11:41:03 +0200

    BUS-825-Youtube-Video

---
+++ components/cms/block/CmsBlockYoutubeVideo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { Schemas } from '@shopware/api-client/api-types';
+const props = defineProps<{
+    block: Schemas['CmsBlock'];
+}>();
+const { getSlotContent } = useCmsBlock(props.block);
+const video: Schemas['CmsSlot'] = getSlotContent('video');
+</script>
+
+<template>
+    <CmsLoader :content="video" />
+</template>
