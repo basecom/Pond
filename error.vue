c6c58324d25bb7e8933727ca64cc9485e3f581a2
Magnus Borgmann <m.borgmann@basecom.de>
2024-09-18 14:44:19 +0200

    add default layout to error page

--- error.vue
+++ error.vue
@@ -4,16 +4,40 @@ import type { NuxtError } from '#app';
 defineProps<{
     error: NuxtError;
 }>();
+
+const customerStore = useCustomerStore();
+const { refreshCart } = useCart();
+const { loading } = storeToRefs(customerStore);
+
+customerStore.refreshContext();
+refreshCart();
 </script>
 
 <template>
-    <div class="container flex flex-col items-center justify-center">
-        <h1>{{ error.statusCode }} - {{ error.message }}</h1>
-        <NuxtLink
-            class="inline-flex max-w-56 items-center justify-center bg-brand-primary px-4 py-2 text-center text-white hover:bg-brand-primary-dark"
-            :to="{ name: 'index' }"
-        >
-            Homepage
-        </NuxtLink>
-    </div>
+    <NuxtLoadingIndicator />
+    <UtilityLoadingSpinner v-if="loading" />
+
+    <LayoutHeader v-show="!loading" />
+
+    <UtilityToastNotifications />
+
+    <main
+        v-show="!loading"
+        class="mt-4 w-screen"
+    >
+        <div class="container flex flex-col items-center justify-center">
+            <h1 class="pb-4">
+                {{ error.statusCode }} - {{ error.message }}
+            </h1>
+
+            <NuxtLink
+                class="inline-flex max-w-56 items-center justify-center bg-brand-primary px-4 py-2 text-center text-white hover:bg-brand-primary-dark"
+                :to="{ name: 'index' }"
+            >
+                Homepage
+            </NuxtLink>
+        </div>
+    </main>
+
+    <LayoutFooter v-show="!loading" />
 </template>
