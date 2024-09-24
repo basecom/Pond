68231222c8ac4251a5906675132a7083d4ee7c9b
Birte Driehaus <b.driehaus@basecom.de>
2024-09-24 16:20:07 +0200

    [BUS-837] code review changes

--- components/Cms/Element/CmsElementHtml.vue
+++ components/Cms/Element/CmsElementHtml.vue
@@ -4,10 +4,13 @@ import type { CmsElementText } from '@shopware-pwa/composables-next';
 const props = defineProps<{
     element: CmsElementText;
 }>();
+
+const config = useCmsElementConfig(props.element);
+const content = config.getConfigValue('content');
 </script>
 
 <template>
     <!-- v-html is necessary because we need to render html -->
     <!-- eslint-disable vue/no-v-html -->
-    <div v-html="props.element.data.content" />
+    <div v-html="content" />
 </template>
