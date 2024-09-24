cc5897c514ac80f45ef64c1aa70854a0473a8345
carojansing <88533476+carojansing@users.noreply.github.com>
2024-09-24 12:34:20 +0200

    Update components/cms/block/CmsBlockYoutubeVideo.vue
    Co-authored-by: nele sophie <53444487+aheartforspinach@users.noreply.github.com>

--- components/cms/block/CmsBlockYoutubeVideo.vue
+++ components/cms/block/CmsBlockYoutubeVideo.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import type { Schemas } from '@shopware/api-client/api-types';
+
 const props = defineProps<{
     block: Schemas['CmsBlock'];
 }>();
+
 const { getSlotContent } = useCmsBlock(props.block);
 const video: Schemas['CmsSlot'] = getSlotContent('video');
 </script>
