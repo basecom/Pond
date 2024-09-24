cfcb91f5f7340ffcd2ca41bb6520f6d72485f38b
Birte Driehaus <b.driehaus@basecom.de>
2024-09-24 11:46:53 +0200

    [BUS-837] add html cms block and element

---
+++ components/Cms/Element/CmsElementHtml.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { CmsElementText } from '@shopware-pwa/composables-next';
+
+const props = defineProps<{
+    element: CmsElementText;
+}>();
+</script>
+
+<template>
+    <!-- v-html is necessary because we need to render html -->
+    <!-- eslint-disable vue/no-v-html -->
+    <div v-html="props.element.data.content" />
+</template>
