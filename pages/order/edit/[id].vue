46a06e3be00f5dc30d39ef5fefef060daba0843e
Christian Finkhäuser <c.finkhaeuser@basecom.de>
2024-10-04 19:19:11 +0200

    add a link to order/edit/[id] page for changing the payment method

---
+++ pages/order/edit/[id].vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+//TODO show Order Details like in localhost/order/edit/[orderId] (similar to checkout/finish/[id], add Payment Methods, add update Payment Method option
+</script>
+<template>
+</template>
