d0a89c0f62848c5c1cebd318d5aa9284a6ed3eaa
Marie Löffler <m.loeffler@basecom.de>
2024-10-08 19:25:38 +0200

    add get function

--- composables/usePluginConfig.ts
+++ composables/usePluginConfig.ts
@@ -1,18 +1,19 @@
 
 export function usePluginConfig() {
     const { apiClient } = useShopwareContext();
-    const _configValues = ref({});
+    const configValues = ref({});
 
     const loadConfig = async () => {
         return useAsyncData('pluginConfiguration', async () => {
             const data = (await apiClient.invoke('loadConfig get /pond/config')).data;
-            _configValues.value = data;
+            configValues.value = data;
             return data;
         });
     };
 
     return {
         loadConfig,
-        configValues: computed(() => _configValues.value),
+        // configValues: computed(() => _configValues.value),
+        configValues,
     };
 }
--- stores/ConfigStore.ts
+++ stores/ConfigStore.ts
@@ -1,9 +1,23 @@
 export const useConfigStore = defineStore('config', () => {
-    const { loadConfig, fetchConfig, configValues } = usePluginConfig();
+    const { loadConfig, configValues } = usePluginConfig();
+
+    const get = (key: string) => {
+        if (!configValues.value) {
+            console.warn('[Pond]: config values not loaded');
+            return undefined;
+        }
+
+        if (!configValues.value[key]) {
+            console.warn(`[Pond]: The configuration for '${key}' was not found`);
+            return undefined;
+        }
+
+        return configValues.value[key]
+    }
 
     return {
         loadConfig,
-        fetchConfig,
         configValues,
+        get
     }
 })
