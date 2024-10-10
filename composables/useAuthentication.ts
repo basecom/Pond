eeb04e33a76e8cce7795644158ba33ada8093202
Birte Driehaus <b.driehaus@basecom.de>
2024-10-10 15:58:48 +0200

    [BUS-809] fix auth, reroute from /login and /register if user is logged in

--- composables/useAuthentication.ts
+++ composables/useAuthentication.ts
@@ -1,13 +1,29 @@
 export function useAuthentication() {
-    const { signedIn } = useCustomerStore();
+    const { signedIn, loading } = storeToRefs(useCustomerStore());
+    const { refreshContext } = useCustomerStore();
 
-    const authenticate = () => {
-        if (!signedIn) {
-            navigateTo('/account/login');
+    const rerouteIfLoggedOut = async (targetRoute: string = '/account/login') => {
+        await sessionContextLoaded();
+        if (!signedIn.value) {
+            navigateTo(targetRoute);
         }
     };
 
+    const rerouteIfLoggedIn = async (targetRoute: string = '/account') => {
+        await sessionContextLoaded();
+        if (signedIn.value) {
+            navigateTo(targetRoute);
+        }
+    }
+
+    const sessionContextLoaded = async () => {
+        if (loading.value) {
+            await refreshContext();
+        }
+    }
+
     return {
-        authenticate,
+        rerouteIfLoggedIn,
+        rerouteIfLoggedOut,
     };
 }
