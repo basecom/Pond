135b0c13ac51014cdddae3e80e70661a264b2dea
Magnus Borgmann <m.borgmann@basecom.de>
2024-10-14 15:47:48 +0200

    remove sharedbanner and replace it with already existing static notification, replace non existing snippet

--- components/Cms/Element/CmsElementProductListing.vue
+++ components/Cms/Element/CmsElementProductListing.vue
@@ -75,9 +75,12 @@ productListingCriteriaStore.setSearchResult(getCurrentListing.value, true);
             @update-page="currentPage => changePage(currentPage)"
         />
     </div>
-    <SharedBanner
+
+    <UtilityStaticNotification
         v-else
+        :id="props.element.id + '-no-products-found'"
         type="info"
         :message="$t('cms.element.product.noProductsFound')"
+        class="mt-4"
     />
 </template>
