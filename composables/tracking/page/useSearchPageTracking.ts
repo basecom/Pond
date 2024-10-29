cfa36128fa3ea9a285637a010b45adcddf8ccf66
Adnan Saadeddine <adnan.saadeddine@dialogdata.de>
2024-10-29 15:22:45 +0200

    Feature/TQLG-57 fix naming

--- composables/tracking/page/useSearchPageTracking.ts
+++ composables/tracking/page/useSearchPageTracking.ts
@@ -3,11 +3,11 @@ import type { UseAnalyticsReturn } from '../../../types/analytics/analytics';
 export function useSearchPageTracking(analytics: UseAnalyticsReturn) {
     const { getElements } = useProductSearchListing();
     const { sessionContext } = useSessionContext();
-    const isPlpPageReady = computed(() => !!getElements.value.length && !!sessionContext.value);
+    const isSearchPageReady = computed(() => !!getElements.value.length && !!sessionContext.value);
 
     usePageTracking(analytics, 'search');
     whenever(
-        isPlpPageReady,
+        isSearchPageReady,
         () => {
             analytics.trackViewItemList(getElements.value);
         },
