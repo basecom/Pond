179ca41499fe791001a7216fb9344bf2e4e48038
Adnan Saadeddine <adnan.saadeddine@dialogdata.de>
2024-11-04 18:28:37 +0200

    fix cart refresh tracking

--- composables/tracking/page/useCartPageTracking.ts
+++ composables/tracking/page/useCartPageTracking.ts
@@ -3,12 +3,13 @@ import type { UseAnalyticsReturn } from '../../../types/analytics/analytics';
 export function useCartPageTracking(analytics: UseAnalyticsReturn) {
     const cartItemsCountStore = useCartItemsStore();
     const navigationStore = useNavigationStore();
+    const { cart } = useCart();
     const { mainNavigationElements } = storeToRefs(navigationStore);
     const { cartItemsCount } = storeToRefs(cartItemsCountStore);
-    const isCartPageReady = computed(() => !!cartItemsCount.value && !!mainNavigationElements.value.length);
+    const isCartPageReady = computed(() => cart.value?.lineItems.length && !!cartItemsCount.value && !!mainNavigationElements.value.length);
 
     usePageTracking(analytics, 'cart');
     whenever(isCartPageReady, () => {
-        setTimeout(() => analytics.trackViewCart());
+        analytics.trackViewCart();
     }, { immediate: true });
 }
