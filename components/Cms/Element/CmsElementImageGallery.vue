8e552e86ef23429d5bcbd80e0fffa1c9d7287988
Magnus Borgmann <m.borgmann@basecom.de>
2024-11-04 09:33:11 +0100

    add more props and custom controls to slider

--- components/Cms/Element/CmsElementImageGallery.vue
+++ components/Cms/Element/CmsElementImageGallery.vue
@@ -28,10 +28,11 @@ const slides = elementData.getData('sliderItems') ?? [];
                 <LayoutSlider
                     :classes="[
                         slides.length > 1 ? 'cursor-grab' : '',
-                        galleryPosition.value === 'underneath' ? 'w-full' : 'w-4/5',
+                        galleryPosition.value === 'underneath' ? 'w-full' : 'w-auto',
+                        'max-w-full md:max-w-[calc(100%-150px-16px)]',
                     ]"
-                    :navigation-dots="navigationDots !== 'None'"
-                    :navigation-arrows="navigationArrows !== 'None'"
+                    :navigation-dots="navigationDots !== 'None' && navigationDots !== 'Keine'"
+                    :navigation-arrows="navigationArrows !== 'None' && navigationArrows !== 'Keine'"
                     :thumbs-swiper="`.thumbnailRef-${element.id}`"
                 >
                     <LayoutSliderSlide
@@ -57,27 +58,27 @@ const slides = elementData.getData('sliderItems') ?? [];
 
                 <LayoutSlider
                     :classes="[
-                        'thumbnailRef-' + element.id,
-                        galleryPosition.value === 'underneath' ? 'w-full' : 'w-1/5',
+                        galleryPosition.value === 'underneath' ? 'w-full' : '',
                         slides.length > 1 ? 'cursor-grab' : '',
                     ]"
                     :navigation-dots="false"
-                    :navigation-arrows="false"
+                    :navigation-arrows="slides.length > thumbnailSlidesPerView"
+                    :vertical-navigation="true"
                     :direction="galleryPosition === 'left' ? 'vertical' : 'horizontal'"
                     :space-between="spaceBetween"
                     :slides-per-view="thumbnailSlidesPerView"
+                    :thumb-ref="`thumbnailRef-${element.id}`"
                 >
                     <LayoutSliderSlide
                         v-for="slide in slides"
                         :key="slide.media.id"
                         class="group"
-                        :class="`min-h-[${minHeight}]`"
                     >
                         <img
                             v-if="slide.media.url"
                             :src="slide.media.url"
                             :alt="slide.translated?.alt ?? $t('cms.element.imageAlt')"
-                            class="h-full w-full object-cover object-center opacity-40 group-[.swiper-slide-thumb-active]:border-2 group-[.swiper-slide-thumb-active]:border-brand-primary group-[.swiper-slide-thumb-active]:opacity-100"
+                            class="object-cover object-center opacity-40 group-[.swiper-slide-thumb-active]:border-2 group-[.swiper-slide-thumb-active]:border-brand-primary group-[.swiper-slide-thumb-active]:opacity-100"
                         />
 
                         <template v-else>
