a913884bb778781b7ecc62e93e41e9b9175a2041
Alin Stanciu <alin.stanciu@dialogdata.de>
2024-12-12 17:14:40 +0200

    Add usage example

--- components/LocaleLink.vue
+++ components/LocaleLink.vue
@@ -2,9 +2,17 @@
 /**
  * # Locale Link
  *
- * Wraps the `<NuxtLink>` component, optionally (by default) formatting links with the current locale.
+ * Wraps the `<NuxtLink>` component, optionally (by default) formatting links with the current locale prefix.
  *
  * `<LocaleLink>` is a drop-in replacement for Nuxt's `<NuxtLink>`, Vue Router's `<RouterLink>`, and HTML's `<a>` tag.
+ *
+ * @example
+ * // If the current locale requires a prefix, links to {domain}/{prefix}/products
+ * // Otherwise, links to {domain}/products
+ * <LocaleLink to="/products"></LocaleLink>
+ * @example
+ * // Link localization can be disabled (e.g. for external links). In this case, the link is not changed in any way.
+ * <LocaleLink to="https://www.google.com/" :format="false"></LocaleLink>
  */
 export default {};
 </script>
