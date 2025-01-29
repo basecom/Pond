d003b07f7aa832462c8370410e3741e7d44429dc
Paula Pop <paula.pop@dialogdata.de>
2025-01-29 10:03:24 +0200

    handle possible undefined

--- helpers/preselectShippingMethod.ts
+++ helpers/preselectShippingMethod.ts
@@ -1,17 +1,19 @@
+import type { Schemas } from "@shopware/api-client/api-types";
+
 export const preselectShippingMethod = async () => {
     const { getShippingMethods, setShippingMethod, selectedShippingMethod } = useCheckout();
     const { refreshCart } = useCart();
 
     const shippingMethods = await getShippingMethods();
 
-    const isPreselectedShippingAvailable = shippingMethods.value.find(method => method.id === selectedShippingMethod.id);
+    const isPreselectedShippingAvailable = shippingMethods.value.find((method: Schemas['ShippingMethod']) => method.id === selectedShippingMethod.value.id);
 
     if (isPreselectedShippingAvailable) {
         return;
     }
 
-    const cheapestMethod = shippingMethods.value.reduce((lowest, current) => {
-        return current.prices[0].currencyPrice[0].gross < lowest.prices[0].currencyPrice[0].gross ? current : lowest;
+    const cheapestMethod = shippingMethods.value.reduce((lowest : Schemas['ShippingMethod'], current: Schemas['ShippingMethod']) => {
+        return current.prices?.[0].currencyPrice?.[0].gross < lowest.prices?.[0].currencyPrice?.[0].gross ? current : lowest;
     });
 
     await setShippingMethod({id: cheapestMethod.id});
