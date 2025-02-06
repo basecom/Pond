6530f97ba20fcac0c592ffae0acbbf9c0748d92d
nele sophie <53444487+aheartforspinach@users.noreply.github.com>
2025-02-06 11:59:36 +0100

    Feature/bus 0 plp refactoring (#106)
    * [BUS-0] add some console logs, add shop name, add favicon, run lint
    
    * [BUS-0] start refactoring
    
    * [BUS-0] add product card skeleton, unify filter and sorting option
    
    * [BUS-0] remove test data
    
    * [BUS-0] revert duplicate changes
    
    * [BUS-0] remove outline
    
    * [BUS-0] add limit instead of fixed size

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,6 +32,14 @@ if (route.path !== '/wishlist' && wishlistEnabled) {
     // If not on wishlist page we fetch for displaying the amount of items in the header
     getWishlistProducts();
 }
+
+const shopName = configStore.get('core.basicInformation.shopName');
+useHead({
+    title: shopName,
+    htmlAttrs: {
+        lang: locale.value,
+    },
+});
 </script>
 
 <template>
