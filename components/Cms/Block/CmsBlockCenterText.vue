9bd90f379f5d4d82101b2d50bd0843ca23649357
Magnus Borgmann <m.borgmann@basecom.de>
2025-02-07 16:40:38 +0100

    Feature/tqlg 423 missing cms blocks (#113)
    * 0.0.26
    
    * add missing cms blocks of the text-image category
    
    * increase package version

---
+++ components/Cms/Block/CmsBlockCenterText.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Schemas } from '@shopware/api-client/api-types';
+
+const props = defineProps<{
+    block: Schemas['CmsBlock'];
+}>();
+
+const { getSlotContent } = useCmsBlock(props.block);
+
+const leftImage: Schemas['CmsSlot'] = getSlotContent('left');
+const centerText: Schemas['CmsSlot'] = getSlotContent('center');
+const rightImage: Schemas['CmsSlot'] = getSlotContent('right');
+</script>
+
+<template>
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <CmsLoader :content="leftImage" />
+
+        <div class="content-center">
+            <CmsLoader :content="centerText" />
+        </div>
+
+        <CmsLoader :content="rightImage" />
+    </div>
+</template>
