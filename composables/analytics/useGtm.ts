f3400ab2826032da13dfce4b63bcb9cdbb0dab1f
neleruess <n.ruess@basecom.de>
2025-02-13 09:58:07 +0100

    [BUS-0] fix promotion issue

--- composables/analytics/useGtm.ts
+++ composables/analytics/useGtm.ts
@@ -1,6 +1,6 @@
 import type { Schemas } from '@shopware/api-client/api-types';
 import type { UseAnalyticsReturn } from '~/types/analytics/Analytics';
-import type { PromotionInfo } from '~/types/analytics/promotion';
+import type { PromotionInfo } from '~/types/analytics/Promotion';
 import type { TrackingLineItemList } from '../tracking/useItemTracking';
 
 export function useGtm(): UseAnalyticsReturn {
