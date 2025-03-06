46fa3ebae83f01dad54e929806b79a4ff68c8614
Paula Pop <paula.pop@dialogdata.de>
2025-03-06 10:41:15 +0200

    rearrange types

--- types/CustomerRegisterParams.d.ts
+++ types/CustomerRegisterParams.d.ts
@@ -1,6 +1,8 @@
 import { components } from '@shopware/api-client/api-types/storeApiTypes.overrides';
 
 export type CustomerRegisterParams = {
-    [key: string]: string | boolean | components["schemas"]["CustomerAddress"]
-                   | Omit<components["schemas"]["CustomerAddress"], "createdAt" | "id" | "customerId" | "firstName" | "lastName">;
+    [key: string]:  string |
+                    boolean |
+                    components["schemas"]["CustomerAddress"] |
+                    Omit<components["schemas"]["CustomerAddress"], "createdAt" | "id" | "customerId" | "firstName" | "lastName">;
 }
