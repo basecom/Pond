f8d0ff3fd3c0f7f5c50135d152f77ed32404fc64
RoggigesBrot <henrik.bendorf@outlook.de>
2025-03-21 19:16:26 +0100

    [POND-6]: Added register page with inner and outer component. Added vee-validate form for general fields for the first five inputs.  Added snippets for those fields.

---
+++ components/account/AccountRegisterInner.vue
@@ -0,0 +1,238 @@
+<script setup lang="ts">
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as z from 'zod';
+
+const { getSalutations, fetchSalutations } = useSalutations();
+const { t } = useI18n();
+const configStore = useConfigStore();
+
+const accountTypes = [
+    { label: t('account.register.accountTypes.private'), value: 'private' },
+    { label: t('account.register.accountTypes.business'), value: 'business' },
+] as const;
+const salutations = ref([]);
+
+const registerSchema = toTypedSchema(z.object({
+    accountType: z
+        .string({
+            required_error: t('account.register.accountTypes.errorGeneral'),
+        }),
+    salutation: z
+        .string({
+            required_error:  t('account.register.salutations.errorGeneral'),
+        }),
+    title: z
+        .string().optional(),
+    firstName: z
+        .string({
+            required_error: t('account.register.firstName.errorGeneral'),
+        }),
+    lastName: z
+        .string({
+            required_error: t('account.login.password.errorGeneral'),
+        }),
+    email: z
+        .string({
+            required_error: t('account.login.email.error'),
+        })
+        .email(),
+    confirmMail: z
+        .string({
+            required_error: t('account.login.email.error'),
+        })
+        .email(),
+    birthdate: z
+        .date({
+            required_error: 'No',
+        }),
+    company: z
+        .string({
+            required_error: 'No',
+        }),
+    department: z
+        .string({
+            required_error: 'No',
+        }),
+    vatNumber: z.string(),
+    password: z
+        .string({
+            required_error: 'No',
+        }),
+    confirmPassword: z
+        .string({
+            required_error: 'No',
+        }),
+    // address: {
+    //     label: 'Street and House number',
+    //     inputProps: {
+    //         type: 'text',
+    //         placeholder: 'Provide the account type',
+    //     },
+    // },
+    // postalCode: {
+    //     label: 'Post Code',
+    //     inputProps: {
+    //         type: 'text',
+    //         placeholder: 'Provide postal code',
+    //     },
+    // },
+    // city: {
+    //     label: 'City',
+    //     inputProps: {
+    //         type: 'text',
+    //         placeholder: 'Provide your city',
+    //     },
+    // },
+    // additionalAddressLine1: {
+    //     label: 'Additional Address line 1',
+    //     inputProps: {
+    //         type: 'text',
+    //     },
+    // },
+    // additionalAddressLine2: {
+    //     label: 'Additional Address line 2',
+    //     inputProps: {
+    //         type: 'text',
+    //     },
+    // },
+    // country: {
+    //     label: 'City',
+    //     inputProps: {
+    //         type: 'text',
+    //         placeholder: 'Provide your city',
+    //     },
+    // },
+    // state: {
+    //     label: 'State',
+    //     inputProps: {
+    //         type: 'text',
+    //         placeholder: 'Provide your state',
+    //     },
+    // },
+    // phone: {
+    //     label: 'Phone number',
+    //     inputProps: {
+    //         type: 'number',
+    //         placeholder: 'Provide your phone number',
+    //     },
+    // },
+}));
+const form = useForm({
+    validationSchema: registerSchema,
+});
+
+const onSubmit = form.handleSubmit(() => {
+    console.log('submitting all');
+});
+
+onBeforeMount(async () => {
+    // Get fresh generated salutations
+    await fetchSalutations();
+    salutations.value = getSalutations.value;
+});
+</script>
+
+<template>
+    <slot name="header">
+        <h1 class="mb-5 text-3xl">{{ $t('account.register.header.main') }}</h1>
+    </slot>
+    <slot name="form">
+        <form @submit="onSubmit">
+            <slot name="generalFields">
+                <div class="generalFields space-y-3">
+                    <slot name="generalFieldsHeader">
+                        <h3 class="text-xl">{{ $t('account.register.header.generalFields') }}</h3>
+                    </slot>
+                    <slot name="generalFieldsFormFields">
+                        <FormField v-if="configStore.get('core.loginRegistration.showAccountTypeSelection')" v-slot="{ componentField }" name="accountType">
+                            <UiFormItem>
+                                <UiFormLabel>{{ $t('account.register.accountTypes.label') }}</UiFormLabel>
+                                <UiSelect v-bind="componentField">
+                                    <UiFormControl>
+                                        <UiSelectTrigger>
+                                            <UiSelectValue :placeholder="$t('account.register.accountTypes.placeholder')" />
+                                        </UiSelectTrigger>
+                                    </UiFormControl>
+                                    <UiSelectContent>
+                                        <UiSelectGroup>
+                                            <UiSelectItem
+                                                v-for="accountType in accountTypes"
+                                                :key="accountType.value"
+                                                :value="accountType.value"
+                                            >
+                                                {{ accountType.label }}
+                                            </UiSelectItem>
+                                        </UiSelectGroup>
+                                    </UiSelectContent>
+                                </UiSelect>
+                            </UiFormItem>
+                        </FormField>
+                        <FormField v-slot="{ componentField }" name="salutation">
+                            <UiFormItem>
+                                <UiFormLabel>{{ $t('account.register.salutations.label') }}</UiFormLabel>
+                                <UiSelect v-bind="componentField">
+                                    <UiFormControl>
+                                        <UiSelectTrigger>
+                                            <UiSelectValue :placeholder="$t('account.register.salutations.placeholder')" />
+                                        </UiSelectTrigger>
+                                    </UiFormControl>
+                                    <UiSelectContent>
+                                        <UiSelectGroup>
+                                            <UiSelectItem
+                                                v-for="salutation in salutations"
+                                                :key="salutation.id"
+                                                :value="salutation.id"
+                                            >
+                                                {{ salutation.translated.displayName }}
+                                            </UiSelectItem>
+                                        </UiSelectGroup>
+                                    </UiSelectContent>
+                                </UiSelect>
+                            </UiFormItem>
+                        </FormField>
+                        <FormField v-if="configStore.get('core.loginRegistration.showTitleField')" v-slot="{ componentField }" name="title">
+                            <UiFormItem>
+                                <UiFormLabel>{{ $t('account.register.title.label') }}</UiFormLabel>
+                                <UiFormControl>
+                                    <UiInput
+                                        type="text"
+                                        v-bind="componentField"
+                                        :placeholder="$t('account.register.title.placeholder')"
+                                        :aria-placeholder="$t('account.register.title.placeholder')"
+                                    />
+                                </UiFormControl>
+                            </UiFormItem>
+                        </FormField>
+                        <FormField v-slot="{ componentField }" name="firstName">
+                            <UiFormItem>
+                                <UiFormLabel>{{ $t('account.register.firstName.label') }}</UiFormLabel>
+                                <UiFormControl>
+                                    <UiInput
+                                        type="text"
+                                        v-bind="componentField"
+                                        :placeholder="$t('account.register.firstName.placeholder')"
+                                        :aria-placeholder="$t('account.register.firstName.placeholder')"
+                                    />
+                                </UiFormControl>
+                            </UiFormItem>
+                        </FormField>
+                        <FormField v-slot="{ componentField }" name="lastName">
+                            <UiFormItem>
+                                <UiFormLabel>{{ $t('account.register.secondName.label') }}</UiFormLabel>
+                                <UiFormControl>
+                                    <UiInput
+                                        type="text"
+                                        v-bind="componentField"
+                                        :placeholder="$t('account.register.secondName.placeholder')"
+                                        :aria-placeholder="$t('account.register.secondName.placeholder')"
+                                    />
+                                </UiFormControl>
+                            </UiFormItem>
+                        </FormField>
+                    </slot>
+                </div>
+            </slot>
+        </form>
+    </slot>
+</template>
---
+++ pages/account/register.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="p-4">
+        <AccountRegister />
+    </div>
+</template>
+
