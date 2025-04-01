c52b0d0c13759036552f8bec81e7df01ece061dc
neleruess <n.ruess@basecom.de>
2025-04-01 10:09:18 +0200

    add fix for navi

--- components/layout/header/navigation/LayoutHeaderNavigation.vue
+++ components/layout/header/navigation/LayoutHeaderNavigation.vue
@@ -3,11 +3,12 @@ import type { Schemas } from '@shopware/api-client/api-types';
 
 defineProps<{
   navigationElements?: Schemas['Category'][];
+  width?: number;
 }>();
 </script>
 
 <template>
     <div class="hidden md:flex">
-        <LayoutHeaderNavigationInner :navigation-elements="navigationElements" />
+        <LayoutHeaderNavigationInner :navigation-elements="navigationElements" :width="width" />
     </div>
 </template>
--- components/layout/header/navigation/LayoutHeaderNavigationInner.vue
+++ components/layout/header/navigation/LayoutHeaderNavigationInner.vue
@@ -4,17 +4,17 @@ import { getTranslatedProperty } from '@shopware/helpers';
 
 defineProps<{
   navigationElements?: Schemas['Category'][];
+  width?: number;
 }>();
 </script>
 
 <template>
     <slot name="header-navigation">
-        <!--        <div v-for="navigationElement in navigationElements" :key="navigationElement.id" class="relative">-->
-        <!--            <slot name="header-navigation-name">-->
-        <!--                {{ getTranslatedProperty(navigationElement, 'name') }}-->
-        <!--            </slot>-->
-        <!--        </div>-->
-
+<!--                <div v-for="navigationElement in navigationElements" :key="navigationElement.id" class="relative">-->
+<!--                    <slot name="header-navigation-name">-->
+<!--                        {{ getTranslatedProperty(navigationElement, 'name') }}-->
+<!--                    </slot>-->
+<!--                </div>-->
         <UiNavigationMenu>
             <UiNavigationMenuList>
                 <UiNavigationMenuItem v-for="navigationElement in navigationElements" :key="navigationElement.id">
@@ -22,7 +22,10 @@ defineProps<{
                       {{ getTranslatedProperty(navigationElement, 'name') }}
                     </UiNavigationMenuTrigger>
                     <UiNavigationMenuContent>
-                        <ul class="grid gap-3 p-6 w-96 max-w-11/12 lg:grid-cols-2">
+                        <ul
+                            class="grid gap-3 p-6 w-96 max-w-11/12 lg:grid-cols-2"
+                            :style="`width: ${width}px;`"
+                        >
                             <li v-for="children in navigationElement.children" :key="children.id">
                                 <UiNavigationMenuLink as-child>
                                     {{ getTranslatedProperty(children, 'name') }}
