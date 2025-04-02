41f4ccfc76bf65733f32468e33f74eef956f87c8
neleruess <n.ruess@basecom.de>
2025-04-02 08:00:12 +0200

    tmp

--- components/layout/header/navigation/LayoutHeaderNavigationInner.vue
+++ components/layout/header/navigation/LayoutHeaderNavigationInner.vue
@@ -10,31 +10,30 @@ defineProps<{
 
 <template>
     <slot name="header-navigation">
-        <!--                <div v-for="navigationElement in navigationElements" :key="navigationElement.id" class="relative">-->
-        <!--                    <slot name="header-navigation-name">-->
-        <!--                        {{ getTranslatedProperty(navigationElement, 'name') }}-->
-        <!--                    </slot>-->
-        <!--                </div>-->
         <UiNavigationMenu>
             <UiNavigationMenuList>
                 <UiNavigationMenuItem v-for="navigationElement in navigationElements" :key="navigationElement.id">
                     <UiNavigationMenuLink v-if="navigationElement.childCount === 0" :class="navigationMenuTriggerStyle()">
-                        <LazyLayoutHeaderNavigationLink :navigation-element="navigationElement" />
+                        <slot name="header-navigation-link">
+                            <LazyLayoutHeaderNavigationLink :navigation-element="navigationElement" />
+                        </slot>
                     </UiNavigationMenuLink>
 
                     <template v-else>
-                        <UiNavigationMenuTrigger>
-                            {{ getTranslatedProperty(navigationElement, 'name') }}
-                        </UiNavigationMenuTrigger>
-                        <UiNavigationMenuContent>
-                            <ul class="max-w-11/12 grid w-96 gap-y-3 p-6 text-sm lg:grid-cols-2">
-                                <li v-for="children in navigationElement.children" :key="children.id">
-                                    <UiNavigationMenuLink :class="navigationMenuTriggerStyle()">
-                                        <LazyLayoutHeaderNavigationLink :navigation-element="children"/>
-                                    </UiNavigationMenuLink>
-                                </li>
-                            </ul>
-                        </UiNavigationMenuContent>
+                        <slot name="header-navigation-parent">
+                            <UiNavigationMenuTrigger>
+                                {{ getTranslatedProperty(navigationElement, 'name') }}
+                            </UiNavigationMenuTrigger>
+                            <UiNavigationMenuContent>
+                                <ul class="max-w-11/12 grid w-96 gap-y-3 p-6 text-sm lg:grid-cols-2">
+                                    <li v-for="children in navigationElement.children" :key="children.id">
+                                        <UiNavigationMenuLink :class="navigationMenuTriggerStyle()">
+                                            <LazyLayoutHeaderNavigationLink :navigation-element="children" />
+                                        </UiNavigationMenuLink>
+                                    </li>
+                                </ul>
+                            </UiNavigationMenuContent>
+                        </slot>
                     </template>
                 </UiNavigationMenuItem>
             </UiNavigationMenuList>
