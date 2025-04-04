d51e0993c4d6cf35d749e98aa93244de77b0cc1c
nele sophie <53444487+aheartforspinach@users.noreply.github.com>
2025-04-04 12:30:21 +0200

    Update components/layout/header/LayoutHeader.vue
    Co-authored-by: JacquelineKlimmek <j.klimmek@basecom.de>

--- components/layout/header/LayoutHeader.vue
+++ components/layout/header/LayoutHeader.vue
@@ -3,7 +3,7 @@ const { sessionContext } = useSessionContext();
 const { loadNavigationElements, navigationElements } = useNavigation();
 
 // we load per maximum four levels
-const navigationCategoryDepth = sessionContext.value?.salesChannel.navigationCategoryDepth <= 4 ? sessionContext.value?.salesChannel.navigationCategoryDepth: 4;
+const navigationCategoryDepth = sessionContext.value?.salesChannel.navigationCategoryDepth <= 4 ? sessionContext.value?.salesChannel.navigationCategoryDepth : 4;
 
 onMounted(async () => {
     await loadNavigationElements({ depth: navigationCategoryDepth });
