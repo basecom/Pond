8b7f1c7a840f01db48034efca0bec95c19e76214
Cao Hoang Long Nguyen <c.nguyen@basecom.de>
2025-04-17 13:24:04 +0200

    POND-14: add link

--- components/error/NotFoundErrorInner.vue
+++ components/error/NotFoundErrorInner.vue
@@ -9,7 +9,7 @@ import UiButton from '../ui/button/UiButton.vue';
             <slot name="subTitle"><p class="mb-4 text-center">{{ $t('error.404.detail') }}</p></slot>
             <slot name="button">
                 <div class="text-center">
-                    <UiButton variant="link">{{ $t('error.404.button') }}</UiButton>
+                  <NuxtLinkLocale to="/"><UiButton variant="link">{{ $t('error.404.button') }}</UiButton></NuxtLinkLocale>
                 </div>
             </slot>
         </div>
