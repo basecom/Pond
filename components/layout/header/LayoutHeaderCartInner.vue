0114b80c0b56c7fa3a5897de0b79dd1047763248
Cao Hoang Long Nguyen <c.nguyen@basecom.de>
2025-04-17 13:21:08 +0200

    POND-8: add buttons

--- components/layout/header/LayoutHeaderCartInner.vue
+++ components/layout/header/LayoutHeaderCartInner.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { Schemas } from '@shopware/api-client/api-types';
+import UiButton from "../../ui/button/UiButton.vue";
 
-withDefaults(
+const props = withDefaults(
     defineProps<{
       cartItems?: Schemas['LineItem'][];
     }>(),
@@ -9,6 +10,8 @@ withDefaults(
         cartItems: undefined,
     },
 );
+
+const hasLineItems = () => props.cartItems && props.cartItems?.length > 0;
 </script>
 
 <template>
@@ -20,7 +23,7 @@ withDefaults(
                 </slot>
 
                 <slot name="cart-badge">
-                    <UiBadge v-if="cartItems?.length > 0" class="absolute -right-2 -top-1.5 px-1 py-0 text-xs font-normal">
+                    <UiBadge v-if="hasLineItems()" class="absolute -right-2 -top-1.5 px-1 py-0 text-xs font-normal">
                         {{ cartItems.length }}
                     </UiBadge>
                 </slot>
@@ -31,7 +34,17 @@ withDefaults(
             <UiSheetHeader>
                 <UiSheetTitle>Cart</UiSheetTitle>
                 <UiSheetDescription>
-                    products
+                    <template v-if="hasLineItems()">
+
+                    </template>
+                  <template v-else>
+
+                  </template>
+                  <div class="flex flex-col">
+                    <div class="text-center"><a href="/"><UiButton variant="link">{{$t('proceedToCheckout')}}</UiButton></a></div>
+                    <div class="text-center"><a href="/"><UiButton variant="link">{{$t('proceedToCart')}}</UiButton></a></div>
+
+                  </div>
                 </UiSheetDescription>
             </UiSheetHeader>
         </UiSheetContent>
